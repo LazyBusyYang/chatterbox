7a4b6310f9aaa08d2d737fbf6e2f760676826771
LazyBusyYang <gaoyang_cn@outlook.com>
2025-11-26 11:53:17 +0000

    Add local configuration file for FastAPI server settings
    - Created `local_config.py` to define logger configuration, server type, and audio prompts for multiple languages.
    - Configured server to enable CORS and set host and port for deployment.

---
+++ dlp3d/local_config.py
@@ -0,0 +1,41 @@
+# CRITICAL = 50
+# ERROR = 40
+# WARNING = 30
+# INFO = 20
+# DEBUG = 10
+# NOTSET = 0
+__logger_cfg__ = dict(
+    logger_name="root",
+    file_level=10,
+    console_level=20,
+    logger_path='logs/server.log',
+)
+
+type = 'FastAPIServer'
+checkpoint_dir = "weights"
+audio_prompts = dict(
+    keqing_zh=dict(
+        name="刻晴-中文",
+        language_id="zh",
+        path="data/keqing_zh_12min.wav"
+    ),
+    furina_zh=dict(
+        name="芙宁娜-中文",
+        language_id="zh",
+        path="data/furina_zh_16min.wav"
+    ),
+    hutao_zh=dict(
+        name="胡桃-中文",
+        language_id="zh",
+        path="data/hutao_zh_6min.wav"
+    ),
+    trump_en=dict(
+        name="特朗普-英文",
+        language_id="en",
+        path="data/trump_en_2min.wav"
+    ),
+)
+enable_cors = True
+host = '0.0.0.0'
+port = 18085
+logger_cfg = __logger_cfg__
